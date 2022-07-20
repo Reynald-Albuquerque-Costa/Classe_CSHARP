967d897a9090d6d11c4b48093c7d7635f6562a52
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-07-20 00:27:34 -0300

    Exercício - Classe_CSHARP

---
+++ Classe/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classe.Src;
+using System;
+
+namespace Classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time SaoPaulo = new Time("São Paulo", Categoria.SerieA, 1930, "São Paulo");
+            Time Cruzeiro = new Time("Cruzeiro", Categoria.SerieB, 1921, "Minas Gerais");
+            Time Paysandu = new Time("Paysandu", Categoria.SerieC, 1914, "Para");
+            Time PousoAlegre = new Time("Pouso Alegre", Categoria.SerieD, 1913, "Minas Gerais");
+
+            Console.WriteLine(SaoPaulo.ToString());
+
+        }
+    }
+}
---
+++ Classe/Src/Time.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe.Src
+{
+    internal class Time
+    {
+        public string Nome { get; set; }
+
+        Categoria Divisao { get; set; }
+
+        public int AnoOrigem { get; set; }
+
+        public string Estado { get; set; }
+
+        public Time(string nome, Categoria divisao, int anoOrigem, string regiao)
+        {
+            Nome = nome;
+            Divisao = divisao;
+            AnoOrigem = anoOrigem;
+            Estado = regiao;
+        }
+
+        public override string ToString()
+        {
+            return $"\nO nome é: {Nome}" +
+                   $"\nA Série que ele atua é: {Divisao}" +
+                   $"\nO ano que nasceu: {AnoOrigem}" +
+                   $"\nO estado onde fica é: {Estado}";
+
+        }
+    }
+    
+    public enum Categoria
+    {
+        SerieA,
+        SerieB,
+        SerieC,
+        SerieD
+
+    }
+}
